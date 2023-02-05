57d30166e6c417ba42b5dd536526ca81423527bd
Jaden Chant <chant1@stolaf.edu>
2023-02-05 16:13:49 -0600

    Added initial homepage

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  title: 'Jaden Chant',
+  theme: {
+    dark: true,
+    colors: {
+      primary: '#ff0000',
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Jaden Chant</h1>
+  </div>
+</template>
