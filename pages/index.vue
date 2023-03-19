00d80530ae1861913ebdf361624fd0e140938720
Jaden Chant <chant1@stolaf.edu>
2023-03-18 19:13:56 -0500

    add dotenv package

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Jaden Chant</h1>
+    <h2>What up</h2>
   </div>
 </template>
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-const config = useRuntimeConfig();
+dotenv.config();
+
+// const config = useRuntimeConfig();
 mongoose.set('strictQuery', false);
 
 export default async () => {
   try {
-    await mongoose.connect(config.mongoUrl);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('DB connection established.');
   } catch (err) {
     console.error('DB connection failed.', err);
