5cd9259cc16df2cad8d024b0cc11424ecf2bffae
Jaden Chant <chant1@stolaf.edu>
2023-11-17 10:24:24 -0600

    formatting health data

--- pages/health.vue
+++ pages/health.vue
@@ -1,8 +1,14 @@
 <template>
   <title>Health Data</title>
   <div>
-    <h2 class="text-2xl mb-4">Health Data</h2>
-    <p v-if="!pending && !error">{{ data }}</p>
+    <h2 class="text-2xl mb-4">Health Data {{ formatDate() }}</h2>
+    <div v-if="!pending && !error" class="text-xl">
+      <p>
+        Distance: {{ data.distance[0].distance }} {{ data.distance[0].units }}
+      </p>
+      <p>Flights: {{ data.flights[0].flights }} flights</p>
+      <p>Steps: {{ data.steps[0].steps }} steps</p>
+    </div>
     <p v-if="pending">Loading</p>
     <p v-if="error">Error: {{ error.message }}</p>
   </div>
@@ -14,5 +20,13 @@ definePageMeta({
 });
 
 const { data, pending, error } = await useFetch('/api/health');
-console.log(data);
+
+function formatDate() {
+  const date = new Date();
+  const year = date.toLocaleString('default', { year: 'numeric' });
+  const month = date.toLocaleString('default', { month: '2-digit' });
+  const day = date.toLocaleString('default', { day: '2-digit' });
+
+  return `${month}/${day - 1}/${year}`;
+}
 </script>
