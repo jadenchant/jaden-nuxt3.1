ecfc6bf2f121416832a18cf1eb4405b0fa2fa608
Jaden Chant <chant1@stolaf.edu>
2024-02-21 22:02:17 -0600

    Update index.vue with Jaden's introduction

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,27 @@
     src="/images/jaden_nyc.png"
     class="absolute z-0 right-0 bottom-0 md:top-16 sm:h-[500px] md:h-[750px]"
   />
-  <div>
-    <h2 class="text-2xl mb-4"></h2>
-    <div class="flex flex-row-reverse"></div>
+  <div class="relative z-30">
+    <h2 class="text-2xl md:text-4xl mb-6">Hi, I'm Jaden!</h2>
+    <div
+      class="flex flex-col text-lg ml-2 md:ml-4 lg:h-56 lg:justify-between lg:text-2xl"
+    >
+      <p class="mb-4">
+        I am a Senior Student @ St. Olaf College in Northfield, MN.
+      </p>
+      <p class="mb-4">
+        I am looking for a career in Front-End Web Development.
+      </p>
+      <p class="mb-4">
+        If you would like to contact me, feel free to message me on
+        <NuxtLink
+          to="https://www.linkedin.com/in/jaden-chant/"
+          target="_blank"
+          class="underline"
+          >LinkedIn</NuxtLink
+        >!
+      </p>
+    </div>
   </div>
 </template>
 
