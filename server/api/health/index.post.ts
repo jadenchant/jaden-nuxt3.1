72d8fb075cc87ae903f8203cdb4b3a3a4cadd939
Jaden Chant <chant1@stolaf.edu>
2024-07-14 18:49:01 -0500

    fix health api: allow for default values

--- server/api/health/index.post.ts
+++ server/api/health/index.post.ts
@@ -5,45 +5,86 @@ export default defineEventHandler(async (event: any) => {
   console.log('POST /api/health');
 
   try {
-    const health_api_key = getHeader(event, 'HEALTH_API');
+    const healthAPIKey = getHeader(event, 'HEALTH_API');
 
-    if (process.env.HEALTH_API !== health_api_key) {
+    if (process.env.HEALTH_API !== healthAPIKey) {
       throw new Error('INVALID API KEY');
     } else {
       const body = await readBody(event);
       const data = body.data.metrics;
-      console.log('body:', JSON.stringify(body));
-      console.log('data:', JSON.stringify(data));
 
       let flights, steps, distance;
 
-      for (let i = 0; i < 3; i++) {
+      let currentDate;
+
+      const hasData = new Map();
+      hasData.set('flights', false);
+      hasData.set('steps', false);
+      hasData.set('distance', false);
+
+      for (let i = 0; i < data.length; i++) {
         if (data[i].name === 'flights_climbed') {
           flights = data[i];
+          currentDate = flights.data[0].date;
+          hasData.set('flights', true);
         } else if (data[i].name === 'step_count') {
           steps = data[i];
+          currentDate = steps.data[0].date;
+          hasData.set('steps', true);
         } else if (data[i].name === 'walking_running_distance') {
           distance = data[i];
+          currentDate = distance.data[0].date;
+          hasData.set('distance', true);
         }
       }
 
-      const flightsData = new Flights({
-        date: flights.data[0].date,
-        flights: flights.data[0].qty,
-        units: flights.units,
-      });
+      let flightsData;
 
-      const stepsData = new Steps({
-        date: steps.data[0].date,
-        steps: round(steps.data[0].qty),
-        units: steps.units,
-      });
+      if (!hasData.get('flights')) {
+        flightsData = new Flights({
+          date: flights.data[0].date,
+          flights: flights.data[0].qty,
+          units: 'flights',
+        });
+      } else {
+        flightsData = new Flights({
+          date: currentDate,
+          flights: 0,
+          units: 'flights',
+        });
+      }
 
-      const distanceData = new Distances({
-        date: distance.data[0].date,
-        distance: round(distance.data[0].qty),
-        units: distance.units,
-      });
+      let stepsData;
+
+      if (!hasData.get('steps')) {
+        stepsData = new Steps({
+          date: steps.data[0].date,
+          steps: 0,
+          units: 'steps',
+        });
+      } else {
+        stepsData = new Steps({
+          date: currentDate,
+          steps: 0,
+          units: 'steps',
+        });
+      }
+
+      let distanceData;
+
+      if (!hasData.get('distance')) {
+        distanceData = new Distances({
+          date: distance.data[0].date,
+          distance: round(distance.data[0].qty),
+          units: 'mi',
+        });
+      } else {
+        distanceData = new Distances({
+          date: currentDate,
+          distance: 0,
+          units: 'mi',
+        });
+      }
 
       const flightsSave = await flightsData.save();
       const stepsSave = await stepsData.save();
